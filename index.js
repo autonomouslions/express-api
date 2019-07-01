9ddfbae0255cc92d4121c658f1e3cc78ea6a56b5
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-07-01 23:07:04 +0200

    add points on map

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const request = require("request");
 
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/pois", (req, res) =>
+//   res.send(
+//     request("poidump.json", (err, resp, body) => {
+//       console.log(err);
+//       console.log(body);
+//     })
+//   )
+// );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
