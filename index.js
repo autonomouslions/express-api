cdb35d766bbc7392b6ce224e49968d91579fa8b5
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-07-02 09:30:49 +0200

    add booking status

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 const request = require("request");
 
-app.use(express.static("public"));
+let status = app.use(express.static("public"));
+
+const busy = "#f02c2e";
+const booked = "#ffce5d";
+const free = "#5ac5b2";
 
 // app.get("/pois", (req, res) =>
 //   res.send(
@@ -14,4 +18,32 @@ app.use(express.static("public"));
 //   )
 // );
 
+let spotStatus = free;
+
+app.get("/status", (req, res) => {
+  console.log("called status");
+  res.json({ status: spotStatus });
+});
+
+app.get("/free", (req, res) => {
+  console.log("called free");
+  spotStatus = free;
+  res.sendStatus(200);
+});
+
+app.get("/busy", (req, res) => {
+  console.log("called busy");
+  spotStatus = busy;
+
+  res.sendStatus(200);
+});
+
+app.get("/booked", (req, res) => {
+  console.log("called booked");
+  spotStatus = booked;
+
+  res.sendStatus(200);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//10.1.128.207
